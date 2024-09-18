c9cacf86d788dfd4bda4f32365f2267e28318270
Uma Chandra Sekhara Sarma Thadepalli <chandrasekhara067@gmail.com>
2024-09-18 18:16:43 +0530

    update firebase & layout

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "vivid-voices-d5893.firebaseapp.com",
-  projectId: "vivid-voices-d5893",
-  storageBucket: "vivid-voices-d5893.appspot.com",
-  messagingSenderId: "308749479894",
-  appId: "1:308749479894:web:3f2662f8af875ebaa1fe77",
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
